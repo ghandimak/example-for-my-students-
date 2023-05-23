381f4f01bcc3a74bd298324aebe19de50452dad6
ghandi makarem <makaremgandi@gmail.com>
2023-05-23 12:11:39 +0200

    my first commitgit status

---
+++ Main.js
@@ -0,0 +1,22 @@
+const myfunction = (Text) => {console.log(" you called this function with input:" + Text);
+};
+
+function sumtwonumber(a,b) {
+   return a + b 
+};
+function suumarray(arr){
+    let sum = 0;
+
+    arr.foreach(element) => (sum += element)
+     for (let index = 0; index < arr.length; index++) {
+
+     }
+}
+
+//myfunction ("hello world");
+
+
+const result = sumtwonumber (1,2);
+console.log(result);
+
+
